89a2a80e75c19eb1c9c1d1719a4861ca888e9983
DISHANT <pateldishant474@gmail.com>
2022-10-03 10:04:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/practical3/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.practical3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun GoToNext(view: View) {}
+}
---
+++ app/src/main/java/com/example/practical3/thirdactivity.kt
@@ -0,0 +1,18 @@
+package com.example.practical3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class thirdactivity : AppCompatActivity() {
+    lateinit var textView3: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thirdactivity)
+        textView3=findViewById(R.id.textView3)
+        val intent2=intent
+        val message=intent2.getStringExtra("secondactivity").toString()
+        textView3.text=message
+
+    }
+}
